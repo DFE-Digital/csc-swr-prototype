6ed156509b8c7d47e4bb647626f8bb8fdc6e0cad
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-01-13 16:18:01 +0000

    Add deploy.yaml to use terraform to deploy
    Remove navigation links

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "View pages", type: :feature do
   scenario "Navigate to home" do
     visit "/pages/home"
 
-    expect(page).to have_text("Lorem")
+    expect(page).to have_text("First deploy -> Happy Days")
   end
 end
