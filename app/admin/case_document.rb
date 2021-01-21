2a2c49e31f578735c180391baf510bc676c1915a
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-01-21 14:41:32 +0000

    Created Case Document in Active Admin and allowed ability to upload files.

--- app/admin/case_document.rb
+++ app/admin/case_document.rb
@@ -1,9 +1,28 @@
-ActiveAdmin.register Case do
+ActiveAdmin.register CaseDocument do
   menu parent: "Cases", priority: 1
+
+  form title: 'Document upload' do |f|
+    f.inputs 'document' do
+      f.input :case
+      f.input :title
+      f.input :document_type
+      f.input :document, as: :file
+    end
+    f.actions
+  end
 
   controller do
     def permitted_params
       params.permit!
+    end
+  end
+
+  show title: 'Case Document' do 
+    default_main_content
+    attributes_table do 
+      row :image do |ad|
+        image_tag ad.document
+      end
     end
   end
 end
