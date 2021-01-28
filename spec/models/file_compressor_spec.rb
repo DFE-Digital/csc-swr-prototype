63bfe3f52213259a3488b7a89308edfa966e482d
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-01-28 15:29:57 +0000

    Deleted some useless files and cleaned up tests

--- spec/models/file_compressor_spec.rb
+++ spec/models/file_compressor_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe FileCompressor do
+  let(:original_file_size) { File.open(Rails.root.join("spec/fixtures/linux.jpg")).size }
+
   it "can make an instance of itself" do
     @file_compressor = FileCompressor.new
     expect(@file_compressor).to be_a FileCompressor
@@ -23,10 +25,9 @@ RSpec.describe FileCompressor do
       filename: "linux.jpg",
       content_type: "image/jpeg",
     )
-    previous_size_in_bytes = @file_compressor.file.attachment.byte_size
     @file_compressor.save!
     @file_compressor.compress_file
-    expect(@file_compressor.file.attachment.byte_size).to be < previous_size_in_bytes
+    expect(@file_compressor.file.attachment.byte_size).to be < original_file_size
   end
 
   it "can compress a file using GzipWriter" do
@@ -37,12 +38,13 @@ RSpec.describe FileCompressor do
       gz.write IO.binread(Rails.root.join("spec/fixtures/linux.jpg"))
       gz.close
     end
-    previous_size_in_bytes = File.open(Rails.root.join("spec/fixtures/linux.jpg")).size
+    
     @file_compressor.file.attach(
       io: File.open(compressed_file.path),
       filename: zipped_file,
       content_type: "gz",
     )
-    expect(@file_compressor.file.attachment.byte_size).to be < previous_size_in_bytes
+    
+    expect(@file_compressor.file.attachment.byte_size).to be < original_file_size
   end
 end
