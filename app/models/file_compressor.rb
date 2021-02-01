60e57ac817be0b437856ccb06beec9e8711518cb
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-02-01 11:53:17 +0000

    Redacted test suite and added some error handling

--- app/models/file_compressor.rb
+++ app/models/file_compressor.rb
@@ -4,12 +4,12 @@ class FileCompressor < ApplicationRecord
   has_one_attached :file
 
   def compress_file
-    original_file = file
-    compressed_file = Zlib::Deflate.deflate(original_file.attachment.download, Zlib::BEST_COMPRESSION)
+    raise 'You need to attach a file first' if file.attachment == nil
+    compressed_file = Zlib::Deflate.deflate(file.attachment.download, Zlib::BEST_COMPRESSION)
     file.attach(
       io: StringIO.new(compressed_file),
-      filename: original_file.filename.to_s,
-      content_type: original_file.content_type,
+      filename: file.filename.to_s,
+      content_type: file.content_type,
     )
   end
 end
--- spec/models/file_compressor_spec.rb
+++ spec/models/file_compressor_spec.rb
@@ -1,50 +1,59 @@
 require "rails_helper"
 
 RSpec.describe FileCompressor do
-  let(:original_file_size) { File.open(Rails.root.join("spec/fixtures/linux.jpg")).size }
+  let(:original_file) { File.open("spec/fixtures/linux.jpg") }
+  let(:subject) { FileCompressor.new }
 
-  it "can make an instance of itself" do
-    @file_compressor = FileCompressor.new
-    expect(@file_compressor).to be_a FileCompressor
+  context('instantiation') do
+    it { is_expected.to be_valid }
+    it { is_expected.to be_a FileCompressor }
   end
 
-  it "attach a file to itself" do
-    @file_compressor = FileCompressor.new
-    @file_compressor.file.attach(
-      io: File.open(Rails.root.join("spec/fixtures/linux.jpg")),
-      filename: "linux.jpg",
-      content_type: "image/jpeg",
-    )
-    expect(@file_compressor.file).to be_attached
-  end
-
-  it "can compress a file" do
-    @file_compressor = FileCompressor.first_or_create!
-    @file_compressor.file.attach(
-      io: File.open(Rails.root.join("spec/fixtures/linux.jpg")),
-      filename: "linux.jpg",
-      content_type: "image/jpeg",
-    )
-    @file_compressor.save!
-    @file_compressor.compress_file
-    expect(@file_compressor.file.attachment.byte_size).to be < original_file_size
-  end
-
-  it "can compress a file using GzipWriter" do
-    @file_compressor = FileCompressor.first_or_create!
-    zipped_file = "#{Rails.root.join('spec/fixtures/linux.jpg')}.gz"
-
-    compressed_file = Zlib::GzipWriter.open(zipped_file) do |gz|
-      gz.write IO.binread(Rails.root.join("spec/fixtures/linux.jpg"))
-      gz.close
+  context('attachments') do
+    it "has an attached file" do
+      subject.file.attach(
+        io: original_file,
+        filename: File.basename(original_file.path),
+        content_type: File.extname(original_file.path)
+      )
+      expect(subject.file).to be_attached
     end
-    
-    @file_compressor.file.attach(
-      io: File.open(compressed_file.path),
-      filename: zipped_file,
-      content_type: "gz",
-    )
-    
-    expect(@file_compressor.file.attachment.byte_size).to be < original_file_size
+
+    it "doesnt have an attached file by default" do
+      expect(subject.file).to_not be_attached
+    end
+  end
+
+  context('compressing files') do
+    it "can compress a file" do
+      subject.file.attach(
+        io: File.open("spec/fixtures/linux.jpg"),
+        filename: "linux.jpg",
+        content_type: "image/jpeg",
+      )
+      subject.save!
+      subject.compress_file
+      expect(subject.file.attachment.byte_size).to be < original_file.size
+    end
+  
+    it "can compress a file using GzipWriter" do
+      zipped_file = "#{'spec/fixtures/linux.jpg'}.gz"
+      compressed_file = Zlib::GzipWriter.open(zipped_file) do |gz|
+        gz.write IO.binread("spec/fixtures/linux.jpg")
+        gz.close
+      end
+      
+      subject.file.attach(
+        io: File.open(compressed_file.path),
+        filename: zipped_file,
+        content_type: "gz",
+      )
+      
+      expect(subject.file.attachment.byte_size).to be < original_file.size
+    end
+
+    it 'will throw an error if compress is called when no file exists' do
+      expect { subject.compress_file }.to raise_error('You need to attach a file first')
+    end
   end
 end
