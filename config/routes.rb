a28ba5cec5843d8d6e3ce86fb7a4d73f682914d3
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-02-05 15:33:07 +0000

    Add video capability, upload video and play it in the html using video tag

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   patch "/audio/update/:id", to: "audio#update", as: :audio_update
   get "status", to: "audio#status"
 
+  get "/video", to: "video#new"
+  post "/video/upload", to: "video#upload"
+  get "/video/show/:id", to: "video#show", as: :video_show
+
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
