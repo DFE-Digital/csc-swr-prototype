cc4bf06e5c1b716ece67b6640e0ab773f555b303
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-02-09 14:03:23 +0000

    PouchDB now replicates to CouchDB

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -5,16 +5,16 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { initAll } from 'govuk-frontend';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service_worker.js', { scope: "/" })
-      .then(function(registration) {
-        console.log('[ServiceWorker Client]', 'registration successful with scope: ', registration.scope);
-      }, function(err) {
-        console.log('[ServiceWorker Client]', 'registration failed: ', err);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('service_worker.js', { scope: "/" })
+//       .then(function(registration) {
+//         console.log('[ServiceWorker Client]', 'registration successful with scope: ', registration.scope);
+//       }, function(err) {
+//         console.log('[ServiceWorker Client]', 'registration failed: ', err);
+//       });
+//   });
+// }
 
 Rails.start();
 Turbolinks.start();
